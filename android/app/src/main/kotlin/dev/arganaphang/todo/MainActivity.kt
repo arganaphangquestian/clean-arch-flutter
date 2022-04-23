c2d7efc6532eaf922fccb07a639a6f854eec5aba
arganaphangquestian <arganaphangquestian@gmail.com>
2022-04-23 17:42:52 +0700

    Initial Commit :fire:

---
+++ android/app/src/main/kotlin/dev/arganaphang/todo/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.arganaphang.todo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
